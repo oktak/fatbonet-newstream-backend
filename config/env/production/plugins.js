e21b5ff47fcd822cad663ccb61f9ea52ef701a3c
oktak <oktak@users.noreply.github.com>
2024-11-13 22:22:11 +0800

    feat(upload): S3 provider update config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,44 +1,27 @@
 // File: config/env/production/plugins.js
 
 module.exports = ({ env }) => ({
-  // upload: {
-  //   config: {
-  //     provider: 'aws-s3',
-  //     providerOptions: {
-  //       s3Options: {
-  //         credentials: {
-  //           accessKeyId: env('S3_ACCESS_KEY_ID'),
-  //           secretAccessKey: env('S3_ACCESS_SECRET'),
-  //         },
-  //         region: env('S3_REGION'),
-  //         endpoint: 'https://' + env('S3_ENDPOINT'),
-  //         params: {
-  //           ACL: 'private',
-  //           Bucket: env('S3_BUCKET'),
-  //         },
-  //       },
-  //     },
-  //   },
-  // },
   upload: {
     config: {
       provider: 'aws-s3',
       providerOptions: {
-        accessKeyId: env('S3_ACCESS_KEY_ID'),
-        secretAccessKey: env('S3_ACCESS_SECRET'),
+        credentials: {
+          accessKeyId: env('S3_ACCESS_KEY_ID'),
+          secretAccessKey: env('S3_ACCESS_SECRET'),
+        },
         region: env('S3_REGION'),
-        endpoint: 'https://' + env('S3_ENDPOINT'),
+        baseUrl: `https://s3.`+env('S3_REGION')+`.amazonaws.com/` + env('S3_BUCKET'),
         params: {
-          ACL: env('S3_ACL', 'public-read'),
-          signedUrlExpires: env('S3_SIGNED_URL_EXPIRES', 15 * 60),
+          ACL: env('AWS_ACL', 'private'),
+          signedUrlExpires: env('AWS_SIGNED_URL_EXPIRES', 15 * 60),
           Bucket: env('S3_BUCKET'),
         },
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
+    },
+    actionOptions: {
+      upload: {},
+      uploadStream: {},
+      delete: {},
     },
   },
 })
